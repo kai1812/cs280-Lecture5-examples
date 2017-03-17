c275d8d03e1cc31ff5d20beb13bb694186c6a222
FrankLikuohao <frank.likuohao@gmail.com>
2017-03-17 21:38:54 +0800

    建構式

--- CS280-Lecture5-examples/Example01/Product.cs
+++ CS280-Lecture5-examples/Example01/Product.cs
@@ -1,7 +1,21 @@
 ﻿using System;
+
+using System;
+
 using System.Collections.Generic;
+
+using System.Collections.Generic;
+
 using System.Linq;
+
+using System.Linq;
+
 using System.Text;
+
+using System.Text;
+
+using System.Threading.Tasks;
+
 using System.Threading.Tasks;
 
 namespace StoreLibrary
